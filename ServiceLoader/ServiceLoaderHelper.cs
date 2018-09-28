a6dbebfc861b31b4ff10ccb8a9a4e85a831008c1
Dvir Reich <38475641+dvireich@users.noreply.github.com>
2018-09-28 12:18:18 +0300

    Change the ip to the server pc ip after port forword

--- ServiceLoader/ServiceLoaderHelper.cs
+++ ServiceLoader/ServiceLoaderHelper.cs
@@ -59,7 +59,7 @@ namespace ServiceLoader
         private static ServiceHost InitializeCORESServiceReferences<TC, TI>(string endpointName)
         {
             //Put Public ip of the server computer
-            var endpoint = $"http://192.168.1.25:80/{endpointName}";
+            var endpoint = $"http://192.168.1.14:80/{endpointName}";
             var uri = new Uri(endpoint);
 
             var serviceHost = new CorsEnabledServiceHost(typeof(TC), new []{uri});
